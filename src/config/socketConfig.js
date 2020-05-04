fd057ed307a395a858129c9c60c37c0214628088
gabrielkf <gabrielkf@gmail.com>
2020-05-04 03:16:17 -0300

    correção das mensagens automáticas para pt-BR

--- src/config/socketConfig.js
+++ src/config/socketConfig.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line import/order
 const app = require('./expressConfig');
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
@@ -22,13 +23,13 @@ io.on('connect', socket => {
     socket.join(user.room);
 
     socket.emit('message', {
-      user: 'admin',
-      text: `${user.name}, welcome to room ${user.room}.`,
+      user: 'Loja',
+      text: `Tire suas dúvidas com ${user.room}.`,
     });
 
     socket.broadcast.to(user.room).emit('message', {
-      user: 'admin',
-      text: `${user.name} has joined!`,
+      user: 'Loja',
+      text: `${user.name} precisa de orientação`,
     });
 
     io.to(user.room).emit('roomData', {
@@ -56,8 +57,8 @@ io.on('connect', socket => {
 
     if (user) {
       io.to(user.room).emit('message', {
-        user: 'Admin',
-        text: `${user.name} has left.`,
+        user: 'Loja',
+        text: `${user.name} saiu do chat.`,
       });
 
       io.to(user.room).emit('roomData', {
@@ -67,10 +68,5 @@ io.on('connect', socket => {
     }
   });
 });
-
-// setInterval(
-//   () => io.emit('time', new Date().toTimeString()),
-//   1000
-// );
 
 module.exports = server;
